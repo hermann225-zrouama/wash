ee7b8c2e343e8ab1eb786867c428682c4aa30170
ZROUAMA FRANCK HERMANN <debah2002@gmail.com>
2024-02-04 00:21:27 +0000

    v1 backend

--- API/db/init.db.js
+++ API/db/init.db.js
@@ -4,7 +4,7 @@ const { Sequelize } = require('sequelize');
 const sequelize = new Sequelize(process.env.DB_NAME || 'WASH_DB', process.env.DB_USER || 'admin', process.env.DB_PASSWORD || 'admin',
 {
   host: process.env.DB_HOST || 'localhost',
-  dialect: 'postgres'
+  dialect: process.env.SGBD || 'postgres'
 });
 
 const testDbConnection = async () => {
